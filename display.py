e1a1b34120410b908659b165152a68f751e0a3a5
romanstrazanec <roman.strazanec007@gmail.com>
2019-01-01 17:24:44 +0100

    improvements #2

--- display.py
+++ display.py
@@ -6,7 +6,9 @@ from dice import Dice
 
 
 def clear_screen():
-    system("cls" if name == 'nt' else 'clear')
+    system("cls" if name == 'nt' else "clear")
+    print("[n]umber of dices | [l]ose | [q]uit")
+    print()
 
 
 def roll():
@@ -19,34 +21,52 @@ def split_in_rows(l, n, print_func):
         print()
 
 
-def display(l, s=3):
-    # print(type(l), print(l))
-    if type(l) == int:
-        rolled = [roll() for _ in range(l + 1)]
+def display(dices, spaces=3):
+    if type(dices) == int:
+        rolled = [roll() for _ in range(dices)]
         split_in_rows(rolled, ceil(len(rolled) ** .5), display)
 
-    elif type(l) == list:
+    elif type(dices) == list:
         for n_layer in range(len(Dice.layers)):
-            for n in l:
-                print(Dice(n).layer(n_layer), end=" " * s)
+            for n in dices:
+                print(Dice(n).layer(n_layer), end=" " * spaces)
             print()
 
 
-def play():
-    n = 5
+def ask_ndices():
     while True:
-        for _ in range(10):
-            clear_screen()
-            display(n)
-        print()
+        try:
+            i = input("n <<< ")
+            return abs(int(i))
+        except ValueError:
+            print("Should be an integer!")
 
-        i = input("[l]ose? | [q]uit? ")
+
+def throw(dices, times=10):
+    for _ in range(times):
+        clear_screen()
+        display(dices)
+    print()
+
+
+def play():
+    dices = 0
+    while True:
+        while dices <= 0:
+            dices = ask_ndices()
+
+        throw(dices)
+
+        i = input(">>> ")
         if i == "q":
             clear_screen()
             break
 
         if i == "l":
-            n = (n - 1) % 6
+            dices -= 1 if dices > 0 else 0
+
+        if i == "n":
+            dices = ask_ndices()
 
 
 if __name__ == "__main__":
